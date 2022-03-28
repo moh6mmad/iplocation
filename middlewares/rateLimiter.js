59b59cab8720dfafe644a11e92d17c1d0c821550
Mohammad <mohammad@lync.me>
2022-03-28 10:19:20 +0200

    Updated rate limit middleware to increase the limit

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit')
 
 const limiter = rateLimit({
 	windowMs: 1 * 60 * 1000, // 1 minutes
-	max: 1, // Limit each IP to 10 requests per `window` (here, per 1 minutes)
+	max: 10, // Limit each IP to 10 requests per `window` (here, per 1 minutes)
     message:
 		'Too many requests has been sent from this IP, please try again after an hour. To increase the limit, please contact hi@iploc.info',
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
